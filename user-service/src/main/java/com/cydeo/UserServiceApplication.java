805177a75594748b0fdf7830b1858ec4e1b2eda6
Abdellatif Zouliga <zuuliga@gmail.com>
2024-03-14 13:38:35 -0400

    added OpenAPIDefinition annotation

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,15 +12,18 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(
+        servers = @Server(url = "/user-service"),
+        info = @Info(title = "Ticketing App", version = "1.0", description = "User Service API")
+)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(UserServiceApplication.class,args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
